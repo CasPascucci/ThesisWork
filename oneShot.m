35533fb111f0a7ae6e3fac23735e66cbf6a3ae67
CasPascucci <caseypascucci@gmail.com>
2025-09-22 11:11:02 -0700

    ReOrg

---
+++ oneShot.m
@@ -0,0 +1,8 @@
+function aTVec = oneShot(PDIx0)
+    % Incoming x0 is nondim, and at PDI
+    % Returns vector of aT values, as aT is dependent on time only
+    % This aT is not very accurate, especially early on as the landing site
+    % gravity is assumed for everywhere during this flight
+
+end
+
