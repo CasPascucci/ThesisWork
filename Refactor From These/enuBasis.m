3b459dcfe8bdfd6cb3b5c0c25e4a8462c94a6439
CasPascucci <caseypascucci@gmail.com>
2025-10-12 20:50:10 -0700

    Deleted all files from before the rework

--- Refactor From These/enuBasis.m
+++
@@ -1,8 +0,0 @@
-function [E, N, U] = enuBasis(lat, lon)
-    clat = cos(lat); slat = sin(lat);
-    clon = cos(lon); slon = sin(lon);
-
-    U = [clat*clon;   clat*slon; slat];
-    E = [-slon;       clon;      0];
-    N = [-slat*clon; -slat*slon; clat];
-end
