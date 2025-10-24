0b65918a6404b43430ac2acd6232c12d0506cd63
CasPascucci <caseypascucci@gmail.com>
2025-10-23 23:22:13 -0700

    cleanup simpson code

--- simpsonComp13Integral.m
+++ simpsonComp13Integral.m
@@ -1,5 +1,4 @@
 function cost = simpsonComp13Integral(t, y)
-    %tic 
     N = length(t) - 1;
     if mod(N, 2) ~= 0
         error('Simpson''s rule requires an even number of intervals');
@@ -7,5 +6,4 @@ function cost = simpsonComp13Integral(t, y)
     h = (t(end) - t(1)) / N;
     cost = y(1) + y(end) + 4*sum(y(2:2:end-1)) + 2*sum(y(3:2:end-2));
     cost = cost * (h/3);
-    %toc
 end
