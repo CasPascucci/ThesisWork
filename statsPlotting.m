cfe4190551ca729679240f51646e1ab393788294
Casey <caseypascucci@gmail.com>
2025-11-09 16:34:15 -0800

    - Added Progress Bar - Stats for all 4 constraint combinations - Landing Dispersion added to stats

--- statsPlotting.m
+++ statsPlotting.m
@@ -7,7 +7,8 @@ ok   = [Results.exit_ok]' == 1;
 gamma= [Results.gamma]';
 kr   = [Results.kr]';
 tgo  = [Results.tgo]';        % seconds
-fuel = [Results.fuel_opt]';
+fuel_opt = [Results.fuel_opt]';
+fuel_sim = [Results.fuel_sim]';
 coeff1   = [Results.coeff1]';
 coeff2   = [Results.coeff2]';
 coeff3   = [Results.coeff3]';
@@ -16,19 +17,19 @@ coeff4   = [Results.coeff4]';
 nOK = sum(ok); N = numel(Results);
 fprintf('Plotted: %d / %d  (%.1f%%), converged cases\n', nOK, N, 100*nOK/max(N,1));
 
-vars = {'gamma','kr','tgo','fuel_opt','coeff1','coeff2','coeff3','coeff4'};
-Xall = [gamma,kr,tgo,fuel,coeff1,coeff2,coeff3,coeff4];
+vars = {'gamma','kr','tgo','fuel_opt','fuel_sim','coeff1','coeff2','coeff3','coeff4'};
+Xall = [gamma,kr,tgo,fuel_opt,fuel_sim,coeff1,coeff2,coeff3,coeff4];
 %% Histograms
 Xs = Xall(ok,:);
 figure('Name','Histograms (success only)');
-tiledlayout(2,4,'Padding','compact','TileSpacing','compact');
+tiledlayout(3,3,'Padding','compact','TileSpacing','compact');
 for i = 1:numel(vars)
     nexttile; histogram(Xs(:,i), 50); grid on;
     xlabel(vars{i},"Interpreter","none"); ylabel('count'); title(['Hist ' vars{i}],'Interpreter','none');
 end
 %% Boxcharts
 figure('Name','Boxcharts by exit_ok');
-tiledlayout(2,4,'Padding','compact','TileSpacing','compact');
+tiledlayout(3,3,'Padding','compact','TileSpacing','compact');
 
 for i = 1:numel(vars)
     v = [Results.(vars{i})]';
@@ -38,5 +39,12 @@ for i = 1:numel(vars)
     xticklabels("");
 end
 
-
+%% Final Position Dispersion
+% Calculate and plot the final position dispersion
+finalPositions = [Results.final_error]';
+figure('Name','Final Position Dispersion');
+plot3(finalPositions(:,1),finalPositions(:,2),finalPositions(:,3),'x');
+xlabel('East Position'); ylabel('North Position'); zlabel('Up Position');
+title('Final Position Dispersion for Converged Cases');
+grid on;
 end
